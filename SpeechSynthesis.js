6e171523530c1c1feae797f286c77a6a25db7a67
TayBone2305 <tayfun.alexander@gmx.de>
2023-05-05 16:09:31 +0200

    Hardcoden des Inputs

--- SpeechSynthesis.js
+++ SpeechSynthesis.js
@@ -11,35 +11,29 @@
     const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
 
     // The language of the voice that speaks.
-    speechConfig.speechSynthesisVoiceName = "de-DE-AmalaNeural"; 
+    speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural"; 
 
     // Create the speech synthesizer.
     var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
 
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+    var text = "Test Test Test"
 
-    rl.question("Enter some text that you want to speak >\n> ", function (text) {
-      rl.close();
-      // Start the synthesizer and wait for a result.
-      synthesizer.speakTextAsync(text,
-          function (result) {
-        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-          console.log("synthesis finished.");
-        } else {
-          console.error("Speech synthesis canceled, " + result.errorDetails +
-              "\nDid you set the speech resource key and region values?");
-        }
-        synthesizer.close();
-        synthesizer = null;
-      },
-          function (err) {
-        console.trace("err - " + err);
-        synthesizer.close();
-        synthesizer = null;
-      });
-      console.log("Now synthesizing to: " + audioFile);
-    });
+    // Start the synthesizer and wait for a result.
+    synthesizer.speakTextAsync(text,
+      function (result) {
+    if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+      console.log("synthesis finished.");
+    } else {
+      console.error("Speech synthesis canceled, " + result.errorDetails +
+          "\nDid you set the speech resource key and region values?");
+    }
+    synthesizer.close();
+    synthesizer = null;
+  },
+      function (err) {
+    console.trace("err - " + err);
+    synthesizer.close();
+    synthesizer = null;
+  });
+  console.log("Now synthesizing to: " + audioFile);
 }());
